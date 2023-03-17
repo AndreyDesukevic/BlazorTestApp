6555806a321420671b962f5f8bd9a450aef9a63a
AndreyDesukevic <andrejdesukevic@gmail.com>
2023-03-17 17:26:28 +0300

    Update

--- BlazorTestApp.DAL/Migrations/20230317120519_cost.Designer.cs
+++ BlazorTestApp.DAL/Migrations/20230317120519_cost.Designer.cs
@@ -4,6 +4,7 @@ using BlazorTestApp.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BlazorTestApp.DAL.Migrations
 {
     [DbContext(typeof(WebDbContext))]
-    partial class WebDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230317120519_cost")]
+    partial class cost
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -67,8 +69,8 @@ namespace BlazorTestApp.DAL.Migrations
                     b.Property<int>("ClientId")
                         .HasColumnType("int");
 
-                    b.Property<int>("Cost")
-                        .HasColumnType("int");
+                    b.Property<decimal>("Cost")
+                        .HasColumnType("decimal(10,2)");
 
                     b.Property<string>("Description")
                         .IsRequired()
---
+++ BlazorTestApp/Services/Interfaces/IClientService.cs
@@ -0,0 +1,14 @@
+﻿using BlazorTestApp.DAL.DbModels;
+
+namespace BlazorTestApp.Services.Interfaces
+{
+    public interface IClientService
+    {
+        IEnumerable<Client> GetAll();
+        Client GetById(int id);
+        Client GetByName(string name);
+        bool Delete(Client client);
+        bool Create(Client client);
+        Client Save(Client client);
+    }
+}
