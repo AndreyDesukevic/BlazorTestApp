4f050567809ec051b05202b0b872c32e76874aff
AndreyDesukevic <andrejdesukevic@gmail.com>
2023-03-17 04:23:25 +0300

    UpdateComponents

--- BlazorTestApp.DAL/DbModels/Client.cs
+++ BlazorTestApp.DAL/DbModels/Client.cs
@@ -1,18 +1,27 @@
 ﻿using BlazorTestApp.DAL.Enum;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BlazorTestApp.DAL.DbModels
 {
-    public class Client:BaseModel
+    public class Client : BaseModel
     {
+        [Required]
+        [StringLength(16, ErrorMessage = "FirstName is too long.")]
         public string? FirstName { get; set; }
+
+        [Required]
+        [StringLength(16, ErrorMessage = "LastName is too long.")]
         public string? LastName { get; set; }
         public DateTime DateCreate { get; set; }
         public ClientStatus ClientStatus { get; set; }
         public List<Order> Orders { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Description is too long.")]
         public string? Description { get; set; }
        
     }
